7d25cef2770b34f8c7141e232b0d03fa0583a787
Crowdbotics <engineering@crowdbotics.com>
2023-08-07 23:28:46 +0000

    Initial commit.

---
+++ backend/dawn_wood_dev_107851/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dawn_wood_dev_107851 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dawn_wood_dev_107851.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-wood-dev-107851",
+  url: "https://dawn-wood-dev-107851.botics.co",
+  api: "https://dawn-wood-dev-107851.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
